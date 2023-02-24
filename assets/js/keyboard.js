95252a51c91fc8cd28e79c192446bea9ea81387d
Abid Hasan Sajid <87300812+AbidHasanSajid@users.noreply.github.com>
2023-02-24 11:44:22 +0600

    Up To Die

---
+++ assets/js/keyboard.js
@@ -0,0 +1,26 @@
+/**
+* Template Name: About Page - v5
+* Author: abidhasansajid.me
+* License: https://github.com/license
+*/
+// Disable right-click
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
+
+
+// made by sajid
